4120bb4c6fa84134306b50800993c08e657328a1
Malcolm Morgan <mem48@users.noreply.github.com>
2020-04-15 17:18:07 +0100

    total rebuild

---
+++ R/build_site.R
@@ -0,0 +1,21 @@
+#' Build Site
+#' @noRd
+build_site <- function(){
+  # delete html
+  files_delete <- list.files(pattern = ".html")
+  files_delete <- c(files_delete, list.files(pattern = ".css"))
+  files_delete <- c(files_delete, list.files(pattern = ".js"))
+  files_delete <- c(files_delete, list.files(pattern = ".yml"))
+  files_delete <- c(files_delete, list.files(pattern = ".svg"))
+  unlink(files_delete)
+  unlink("articles", recursive = TRUE)
+  
+  # Build the site
+  pkgdown::build_site()
+  
+  # Copy to home directory
+  rawPath <- "docs"
+  dataPath <- "./"
+  dataFiles <- list.files(rawPath, full.names = TRUE, recursive = TRUE) 
+  file.copy(dataFiles, dataPath, overwrite = TRUE)
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# ITS Leeds on GitHub
+
+This is the central place to find package, code, and courses developed
+by the [Institute for Transport
+Studies](https://environment.leeds.ac.uk/transport) at the [University
+of Leeds](https://www.leeds.ac.uk/), UK.
+
+## ITS Courses
