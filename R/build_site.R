37cd9da6c9bc186ed4e8af0f39f421203f0bf834
Malcolm Morgan <mem48@users.noreply.github.com>
2020-04-15 17:25:01 +0100

    the rest

--- R/build_site.R
+++ R/build_site.R
@@ -9,7 +9,7 @@ build_site <- function(){
   files_delete <- c(files_delete, list.files(pattern = ".svg"))
   unlink(files_delete)
   unlink("articles", recursive = TRUE)
-  
+  dir.create("articles")
   # Build the site
   pkgdown::build_site()
   
